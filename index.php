7791090edc043dfeb5136d5e66a66a2dac43869e
Tim Koop <github@timkoop.com>
2021-05-09 08:00:47 -0500

    Working on Play page.

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ include("head.php");
 <h3>The Rules</h3>
 <ul>
 <li>Players take turns rolling a die.</li>
-<li>When it's one players turn, he can roll as many times as he wants, collecting points.</li>
-<li>When/if he decides to quit rolling, before he rolls a one, all the points he rolled during that turn get added to his total.</li>
-<li>If he rolls a one, he gets no points for that turn and his turn is over.</li>
-<li>First person to 100 points wins.</li>
+<li>When it's a player's turn, he can roll as many times as he wants, collecting points (one point for each number shown on the die).</li>
+<li>When he decides to quit rolling, all the points he rolled during that turn get added to his total.</li>
+<li>But if he rolls a one, he gets no points for that turn and his turn is over.</li>
+<li>The first person to 100 points wins.</li>
 </ul>
 
 
--- play.php
+++ play.php
@@ -4,9 +4,84 @@ include("head.php");
 
 ?>
 
-<h3>Play</h3>
 
-<p>Coming soon...</p>
+<div id="board">
+
+    <div id="left">
+    <h3 style="text-align: center; margin-bottom:0px">You</h3>
+
+    <div style="height:26px"></div>
+
+    <div style="margin-top:20px">
+    Total Score<br>
+    <div class="score" id="youTotal">0</div>
+    </div>
+
+    <div style="margin-top:20px">
+    This Turn (<span id="botRolls">0 rolls</span>)<br>
+    <div class="score" id="youTurn">0</div>
+    </div>
+
+    <div style="margin-top:20px">
+        <button>Roll</button><button style="margin-left:6px">Done</button>
+    </div>
+
+    </div>
+
+
+
+
+    <div>
+    <img src="dice/1.png">
+    </div>
+
+
+
+
+    <div id="right">
+    <h3 style="text-align: center; margin-bottom:0px;">Bot</h3>
+
+    <select style="width: 215px; padding: 3px; height:26px">
+        <option>#1 "Alice", by Tim Koop</optin>
+        <option>#2 "Bob", by Tim Koop</optin>
+        <option>#3 "Super Pig Beater", by Tim Koop</optin>
+    </select>
+
+    <div style="margin-top:20px">
+    Total Score<br>
+    <div class="score" id="botTotal">0</div>
+    </div>
+
+    <div style="margin-top:20px">
+    This Turn (<span id="botRolls">0 rolls</span>)<br>
+    <div class="score" id="botTurn">0</div>
+    </div>
+
+
+    </div>
+
+</div>
+
+
+<style>
+#board {
+    display:flex;
+    justify-content: space-around;
+}
+.score {
+    border: 1px solid black;
+    padding: 12px;
+    text-align: right;
+    width: 190px;
+}
+button {
+    width:105;
+    height:40px;
+    margin-bottom:12px;
+    font-size:larger;
+}
+</style>
+
 
 <?php
 include("tail.php")
