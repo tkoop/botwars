33f7ad29af6ba6adb9abb496e18b712156905720
Tim Koop <github@timkoop.com>
2021-05-14 19:24:12 -0500

    Stuff

--- bot.php
+++ bot.php
@@ -1,12 +1,27 @@
 <?php
 
+include("start.php");
+
 if (isset($_POST["code"])) {
-    var $bot = [
-        "type" => "personal",
-        "passkey" => $_POST["passkey"],
+    $key = preg_replace("/[^A-Za-z0-9]/", '', $_POST["passkey"]);
+
+    if (strlen($key) < 32) {
+        $_SESSION["errorMessage"] = "Your passkey is not long enough.";
+        header("Location: /bot.php");
+        exit();
+    }
+
+    $bot = [
         "code" => $_POST["code"]
     ];
-    header("Location: /bot.php");
+
+    $written = file_put_contents("bots/personal-" . $key . ".json", json_encode($bot));
+    $_SESSION["message"] = "Your bot was saved.";
+
+    if ($_POST["action"] == "Save") header("Location: /bot.php");
+    if ($_POST["action"] == "Save and Play") header("Location: /play.php?id=personal-".$key);
+    if ($_POST["action"] == "Save and Fight") header("Location: /fight.php");
+
     exit();
 }
 
@@ -17,25 +32,18 @@ include("head.php");
 
 <h3>Bring Your Bot To Life!</h3>
 
-<p>
-Your JavaScript code will have access to these variables:
-</p>
+<p>This is how:  Write some JavaScript that returns a true or false, based on whatever variables you want.  A true value means "roll again."  A false value means "Done."  These are the variables you have access to:</p>
 
-<div style="white-space:pre; font-family:mono">const rollNumber;<span class="comment"> // The number of the roll in this turn. The first roll of the turn is 1.</span>
+<p style="white-space:pre; font-family:mono">const rollNumber;<span class="comment"> // The number of the roll in this turn. The first roll of the turn is 1.</span>
 const yourScore;<span class="comment"> // Your current score, not including points from this turn</span>
 const opponentsScore;<span class="comment"> // Your opponent's current score</span>
 const turnPoints;<span class="comment">  // The total amount of points earned during this turn so far, which may or may not make it into yourScore</span>
-</div>
-
-<p>
-Your code should return a boolean (true or false), which answers the question: Do you want to roll again?
 </p>
 
-<form method="post">
 
-<textarea id="code" name="code">// This simple bot just rolls (up to) two times.
+<form method="post" onsubmit="localStorage.setItem('code', $('#code').val())">
 
-return rollNumber <= 2</textarea><br>
+<textarea id="code" name="code"></textarea><br>
 
 <input type="submit" value="Save" name="action">
 <input type="submit" value="Save and Play" name="action">
@@ -49,6 +57,12 @@ return rollNumber <= 2</textarea><br>
 <script>
 $(() => {
     $("#personalKey").val(localStorage.getItem("key"))
+    var code = localStorage.getItem("code")
+    if (code != null) {
+        $("#code").val(code)
+    } else {
+        $("#code").val("// This simple bot just rolls (up to) two times.\n\nreturn rollNumber <= 2")
+    }
 })
 </script>
 
--- head.php
+++ head.php
@@ -16,10 +16,24 @@
 <img src="logo.png" style="width:100%">
 
 <div class="menu">
-    <a href="/index.php" class="<?= $_SERVER["REQUEST_URI"] == "/index.php" ? "active":"" ?>">About This</a>
+    <a href="/index.php" class="<?= $_SERVER["REQUEST_URI"] == "/index.php" || $_SERVER["REQUEST_URI"] == "/" ? "active":"" ?>">About This</a>
     <a href="/play.php" class="<?= $_SERVER["REQUEST_URI"] == "/play.php" ? "active":"" ?>">Play The Game</a>
     <a href="/bot.php" class="<?= $_SERVER["REQUEST_URI"] == "/bot.php" ? "active":"" ?>">Create Your Bot</a>
     <a href="/fight.php" class="<?= $_SERVER["REQUEST_URI"] == "/fight.php" ? "active":"" ?>">Fight Your Bot</a>
     <a href="/submit.php" class="<?= $_SERVER["REQUEST_URI"] == "/submit.php" ? "active":"" ?>">Submit Your Bot To Competition</a>
 </div>
 
+<?php 
+if (isset($_SESSION["errorMessage"])) {
+    ?>
+    <p style="color:darkred"><?php echo htmlentities($_SESSION["errorMessage"]) ?></p>
+    <?php
+    unset($_SESSION["errorMessage"]);
+} 
+if (isset($_SESSION["message"])) {
+    ?>
+    <p style="color:darkgreen"><?php echo htmlentities($_SESSION["message"]) ?></p>
+    <?php
+    unset($_SESSION["message"]);
+} 
+?>
